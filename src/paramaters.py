2702adb4c6bcd78b459d5520cf07985f300c3fac
Francisco Flores <Francisco.Flores.eng@gmail.com>
2026-02-21 20:09:08 -0800

    Quadrotor Parameters

---
+++ src/paramaters.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class QuadParams:
+	def __init__(self):
+		self.m = 1.5 # mass in kg
+		self.g = 9.81 # gravity in m/s^2
+
+		#inertia matrix
+
+		self.J = np.array([
+			[0.02, 0, 0.01],
+			[0.03, 0.04, 0],
+			[0.04, 0, 0]
+		])
